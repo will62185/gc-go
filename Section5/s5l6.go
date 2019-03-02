aa2f8e8f3b9b9d75a475e90332dbf04247ff0905
Will Woodford <will62185@gmail.com>
2019-03-01 23:09:13 -0500

    filling the bucket

---
+++ Section5/s5l6.go
@@ -0,0 +1,22 @@
+// Constants
+
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	a = 42
+	b = 42.78
+	c = "James Bond"
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%T\n", c)
+}
